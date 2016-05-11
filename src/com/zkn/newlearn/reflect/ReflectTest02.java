7f66737a46d11ea084eff59cfbe79e2be996655a
zknxx <zhangconancomputer@126.com>
2016-05-11 19:24:11 +0800

    提交代码

--- src/com/zkn/newlearn/reflect/ReflectTest02.java
+++ src/com/zkn/newlearn/reflect/ReflectTest02.java
@@ -1,6 +1,5 @@
 package com.zkn.newlearn.reflect;
 
-<<<<<<< HEAD
 import java.io.Serializable;
 
 /**
@@ -9,14 +8,6 @@ import java.io.Serializable;
 public class ReflectTest02 implements Serializable{
 
     public void testVariableArgument(String str,char str1){
-=======
-/**
- * Created by wb-zhangkenan on 2016/5/9.
- */
-public class ReflectTest02 {
-
-    public void testVariableArgument(String str,int str1){
->>>>>>> bb8ccae72d2bc580a884af14f5263624d82cb8d5
         System.out.println(str + "   "+str1);
     }
 }
