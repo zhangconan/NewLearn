e8b1b93ff21cbde2fb446e81dd836f94782ef6de
zknxx <zhangconancomputer@126.com>
2016-05-26 19:05:02 +0800

    提交代码 增加注解类

--- src/main/java/com/zkn/newlearn/annotation/AnnotationTest04.java
+++ src/main/java/com/zkn/newlearn/annotation/AnnotationTest04.java
@@ -6,8 +6,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface AnnotationTest02 {
+@Target({ElementType.TYPE,ElementType.FIELD})
+public @interface AnnotationTest04 {
 	
-	String getUserName();
+	String getAddress();
 }
