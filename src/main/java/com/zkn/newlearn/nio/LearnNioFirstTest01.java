15a57a0de2fe3c953aa30f9665d4fa03ccc17e3e
zknxx <zhangconancomputer@126.com>
2016-08-03 23:00:34 +0800

    提交代码 新增 nio

---
+++ src/main/java/com/zkn/newlearn/nio/LearnNioFirstTest01.java
@@ -0,0 +1,37 @@
+package com.zkn.newlearn.nio;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+
+/**
+ * 学习NIO的第一个例子
+ * @author zkn 2016-07-31
+ *
+ */
+
+public class LearnNioFirstTest01 {
+
+	public static void main(String[] args) {
+		CharBuffer cbfer = CharBuffer.allocate(20);
+		String str = "zhangsanlisi";
+		for(int i=0;i<str.length();i++){
+			cbfer.put(str.charAt(i));
+		}
+		//bbfer.flip();
+		while(cbfer.hasRemaining()){
+			System.out.print(cbfer.get());
+		}
+		System.out.println("");
+		
+		ByteBuffer bbfer = ByteBuffer.allocate(20);
+		String stra = "zhangsanlisi";
+		bbfer.put(stra.getBytes());
+		bbfer.flip();
+		int count = bbfer.remaining();
+		byte[] bytes = new byte[count];
+		for(int i=0;i<count;i++){
+			bytes[i] = bbfer.get();
+		}
+		System.out.println(new String(bytes));
+	}
+}
