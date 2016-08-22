7cae44336f45a79a57a95543eec087981bf0d721
zknxx <zhangconancomputer@126.com>
2016-08-22 21:13:37 +0800

    新增json转Map

---
+++ src/main/java/com/zkn/newlearn/json/JsonToMapTest01.java
@@ -0,0 +1,15 @@
+package com.zkn.newlearn.json;
+
+/**
+ * JSON字符串自动转换
+ * Created by zkn on 2016/8/22.
+ */
+public class JsonToMapTest01 {
+
+    public static void main(String[] args){
+
+        String str = "{\"0\":\"zhangsan\",\"1\":\"lisi\",\"2\":\"wangwu\",\"3\":\"maliu\"}";
+
+        System.out.println(str);
+    }
+}
