4d7b6419eb36e1bd6d9565e32c2ae88dce7a43f2
zknxx <zhangconancomputer@126.com>
2016-12-19 20:29:03 +0800

    "."转义

--- src/test/java/com/zkn/newlearn/tester/base/StringTest01.java
+++ src/test/java/com/zkn/newlearn/tester/base/StringTest01.java
@@ -1,13 +1,16 @@
 package com.zkn.newlearn.tester.base;
 
+import java.util.Arrays;
+
 /**
  * Created by wb-zhangkenan on 2016/8/23.
  */
 public class StringTest01 {
 
     public static void main(String[] args){
-        String str = "sssssssz,ddds,";
-        str = str.substring(0,str.length()-1);
-        System.out.println(str);
+        String str = "sssssssz.ddds..sdsdwe";
+        //str = str.substring(0,str.length()-1);
+        //System.out.println(str);
+        System.out.println(Arrays.toString(str.split("\\.")));
     }
 }
