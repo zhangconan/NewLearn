8b7ebe17a85d89e4ab4faa7331185ab4a44f04dd
zknxx <zhangconancomputer@126.com>
2016-12-26 15:40:37 +0800

    Tomcat第一章源码分析和模拟

---
+++ src/test/java/com/zkn/newlearn/tester/base/StringTest02.java
@@ -0,0 +1,34 @@
+package com.zkn.newlearn.tester.base;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by wb-zhangkenan on 2016/12/23.
+ */
+public class StringTest02 {
+
+    @Test
+    public void test(){
+        String str1 = "李四";
+        test(str1);
+        System.out.println(str1);
+        List<String> list = new ArrayList<>();
+        list.add("zhang");
+        test(list);
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+
+    public void test(String str){
+        System.out.println(str);
+    }
+
+    public void test(List<String> str){
+
+        //str = new ArrayList<>();
+        str.add("lisi");
+    }
+}
