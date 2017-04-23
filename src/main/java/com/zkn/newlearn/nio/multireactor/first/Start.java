2719cf6a25f739a1073d4788ecdc5faab28e6a5d
zknxx <zhangconancomputer@126.com>
2017-04-24 00:04:09 +0800

    新增MultiReactor模型的代码

--- src/main/java/com/zkn/newlearn/nio/multireactor/first/Start.java
+++ src/main/java/com/zkn/newlearn/nio/multireactor/first/Start.java
@@ -6,6 +6,12 @@ import java.util.concurrent.Executors;
 
 /**
  * Created by zkn on 2017/4/23.
+ * 多Selector模型：
+ *  可以有多个Acceptor和Selector，目前这个例子中只是用了一个Acceptor和多个Selector。
+ *  思想是：
+ *      在启动的时候注册一个Acceptor(NioServerBoss),这个注册过程是在ServerBootstrap中实现的，注册多个Reacto(NioServerWoker)。
+ *      AbstractNioSelector不断的轮询取注册的事件，如果是连接事件，则在NioServerBoss中处理，如果是读写事件则在NioServerWorker中处理.
+ *
  */
 public class Start {
 
