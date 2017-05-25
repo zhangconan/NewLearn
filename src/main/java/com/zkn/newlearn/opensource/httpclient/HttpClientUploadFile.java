4712b3ad43bcfdcbe0aef17a0efbc8af97fe353c
zknxx <zhangconancomputer@126.com>
2017-05-26 00:29:03 +0800

    新增HttpClient文件上传

--- src/main/java/com/zkn/newlearn/opensource/httpclient/HttpClientUploadFile.java
+++ src/main/java/com/zkn/newlearn/opensource/httpclient/HttpClientUploadFile.java
@@ -1,11 +1,6 @@
 package com.zkn.newlearn.opensource.httpclient;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
@@ -15,39 +10,108 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.junit.Test;
 
 /**
  * Created by wb-zhangkenan on 2017/5/25.
  * http://www.baeldung.com/httpclient-multipart-upload
+ *
  * @author wb-zhangkenan
  * @date 2017/05/25
  */
 public class HttpClientUploadFile {
 
-    public static void main(String[] args){
-        //创建HttpClient对象
+    public static void main(String[] args) {
+        //inputStreamUpload();
+        //fileUpload();
+    }
+
+    /**
+     * File文件上传
+     */
+    @Test
+    public  void fileUpload() {
+        //构建HttpClient对象
         CloseableHttpClient client = HttpClients.createDefault();
         //构建POST请求
-        HttpPost post = new HttpPost("http://localhost:8088/file/uploadFileAction.do");
-        InputStream inputStream ;
+        HttpPost httpPost = new HttpPost("http://localhost:8003/uploadAndDownload/uploadFileAction");
+        InputStream inputStream = null;
         try {
-            inputStream = new FileInputStream("D:\\tmp\\text.csv");
+            //要上传的文件
+            File file = null; file = new File("G:\\qqq.txt");
+            //构建文件体
+            FileBody fileBody = new FileBody(file,ContentType.MULTIPART_FORM_DATA,"qqq.txt");
+            StringBody stringBody = new StringBody("12",ContentType.MULTIPART_FORM_DATA);
+            HttpEntity httpEntity = MultipartEntityBuilder
+                    .create()
+                    .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
+                    .addPart("uploadFile", fileBody)
+                    .addPart("id",stringBody).build();
+            httpPost.setEntity(httpEntity);
+            //发送请求
+            HttpResponse response = client.execute(httpPost);
+            httpEntity = response.getEntity();
+            if(httpEntity != null){
+                inputStream = httpEntity.getContent();
+                //转换为字节输入流
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Consts.UTF_8));
+                String body = null;
+                while ((body = br.readLine()) != null) {
+                    System.out.println(body);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * InputStream文件上传
+     */
+    @Test
+    public void inputStreamUpload() {
+        //创建HttpClient对象
+        CloseableHttpClient client = HttpClients.createDefault();
+        //构建POST请求   请求地址请更换为自己的。
+        //1)
+        HttpPost post = new HttpPost("http://localhost:8003/uploadAndDownload/uploadFileAction");
+        InputStream inputStream = null;
+        try {
+            //文件路径请换成自己的
+            //2)
+            inputStream = new FileInputStream("G:\\LearnVideo\\text01.txt");
             MultipartEntityBuilder builder = MultipartEntityBuilder.create();
             builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
-            builder.addBinaryBody("uploadFile", inputStream, ContentType.create("multipart/form-data"), "text.csv");
+            //第一个参数为 相当于 Form表单提交的file框的name值
+            //第三个参数是文件名
+            //3)
+            builder.addBinaryBody("uploadFile", inputStream, ContentType.create("multipart/form-data"), "text01.txt");
+            //4)构建请求参数 普通表单项
+            StringBody stringBody = new StringBody("12",ContentType.MULTIPART_FORM_DATA);
+            builder.addPart("id",stringBody);
             HttpEntity entity = builder.build();
             post.setEntity(entity);
             //发送请求
             HttpResponse response = client.execute(post);
             entity = response.getEntity();
-            if(entity != null){
+            if (entity != null) {
                 inputStream = entity.getContent();
                 //转换为字节输入流
                 BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, Consts.UTF_8));
                 String body = null;
-                while((body=br.readLine()) != null){
+                while ((body = br.readLine()) != null) {
                     System.out.println(body);
                 }
             }
@@ -57,6 +121,14 @@ public class HttpClientUploadFile {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if(inputStream != null){
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
