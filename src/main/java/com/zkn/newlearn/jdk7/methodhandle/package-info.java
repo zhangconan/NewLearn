c4b0ad39c4a251495c03beb1ee49efeff8cbf482
zknxx <zhangconancomputer@126.com>
2017-08-29 10:57:06 +0800

    新增MethodHandle

---
+++ src/main/java/com/zkn/newlearn/jdk7/methodhandle/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Created by wb-zhangkenan on 2017/8/23.
+ *
+ * @author wb-zhangkenan
+ * @date 2017/08/23
+ */
+package com.zkn.newlearn.jdk7.methodhandle;
+/**
+ * 方法句柄(Method Handle)是JSR292引入的一个重要概念。
+ */
