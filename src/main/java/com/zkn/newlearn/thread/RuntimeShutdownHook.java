8459004ae99d0c6b62b79b2a0c5bad2e58483e3b
zknxx <zhangconancomputer@126.com>
2017-11-15 22:03:46 +0800

    新增shutdownHook

---
+++ src/main/java/com/zkn/newlearn/thread/RuntimeShutdownHook.java
@@ -0,0 +1,35 @@
+package com.zkn.newlearn.thread;/**
+ * Created by zkn on 2017/11/14.
+ */
+
+import static java.lang.System.out;
+
+/**
+ * @author zkn
+ * @date 2017/11/14 23:00
+ */
+public class RuntimeShutdownHook {
+
+    public static void main(String[] args) {
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->
+                out.println("我被关闭了!")
+        ));
+
+        Thread thread = new Thread(() ->
+                System.out.println("这是第一个线程!"));
+        thread.start();
+        
+        Thread thread2 = new Thread(() ->
+                System.out.println("这是第二个线程!"));
+        thread2.start();
+
+        try {
+            thread.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
